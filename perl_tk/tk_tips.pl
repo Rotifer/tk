b764863e27f09d6dfe707398a55369bfe610336a
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2021-09-14 21:26:21 +0100

    Testing package scope
    Enclosed the package in {} to test package scoping

--- perl_tk/tk_tips.pl
+++ perl_tk/tk_tips.pl
@@ -1,25 +1,26 @@
 package MyApplication;
-use Modern::Perl;
-use Tk;
+{
+	use Modern::Perl;
+	use Tk;
 
-sub new {
-    my $class = shift;
-    my $self  = {};
-    $self->{root}   = shift;
-    bless $self, $class;
-    $self->initialisation();
-    return $self;
+	sub new {
+	    my $class = shift;
+	    my $self  = {};
+	    $self->{root}   = shift;
+	    bless $self, $class;
+	    $self->initialisation();
+	    return $self;
 
+	}
+
+	sub initialisation {
+	    my $self = shift;
+	    my $lbl = $self->{root}->Label(-text => 'Hello, world!');
+	    my $btn = $self->{root}->Button(-text    => 'Quit', -command => sub { exit });
+	    $lbl->grid(-column => 0, -row => 0);
+	    $btn->grid(-column => 1, -row => 0);
+	}
 }
-
-sub initialisation {
-    my $self = shift;
-    my $lbl = $self->{root}->Label(-text => 'Hello, world!');
-    my $btn = $self->{root}->Button(-text    => 'Quit', -command => sub { exit });
-    $lbl->grid(-column => 0, -row => 0);
-    $btn->grid(-column => 1, -row => 0);
-}
-
 my $root = MainWindow->new();
 my $app = MyApplication->new($root);
 MainLoop;
